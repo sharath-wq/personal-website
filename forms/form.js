14865dffbf60b11fe5c788d39f4d9b1638a03a1b
sharath-wq <chandranpsharath@gmail.com>
2023-07-17 11:10:00 +0530

    form-submission added, readme text removed

--- forms/form.js
+++ forms/form.js
@@ -1,11 +1,15 @@
-function sendEmail() {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "chandranpsharath@gmail.com",
-    Password: "14068E8169D2DAA1C841DE9B10EBD6F64FCC",
-    To: "chandranpsharath@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: document.getElementById("subject").value,
-    Body: document.getElementById("message").value,
-  }).then((message) => alert(message));
-}
+$("#submit-form").submit((e) => {
+  e.preventDefault();
+  $.ajax({
+    url: "https://script.google.com/macros/s/AKfycbySS12iM3Vmm-yRx3ueqI8X_fbHZXWszDUj-wo_XT1-1e5C5cXHi6SijTfjF7wUZSQ_/exec",
+    data: $("#submit-form").serialize(),
+    method: "post",
+    success: function (response) {
+      alert("Form submitted successfully");
+      window.location.reload();
+    },
+    error: function (err) {
+      alert("Something Error");
+    },
+  });
+});
